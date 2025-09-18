7dcb543abdfbb259bb6076d8983a134247eae4af
Pato <pwzarauz@gmail.com>
2025-09-17 23:44:13 -0300

    Created the button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import { ComponentProps } from "react"
+import styles from "./index.module.scss"
+
+// Get the props of a native button element
+type NativeButtonProps = ComponentProps<"button">
+
+// Extend the native props with your custom props
+interface ButtonProps extends NativeButtonProps {
+  variant?: "primary"
+  text?: string
+}
+
+const Button = ({ text, variant = "primary", ...rest }: ButtonProps) => {
+  return (
+    <button
+      className={styles.button}
+      data-content={text}
+      data-variant={variant}
+      {...rest}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Button
