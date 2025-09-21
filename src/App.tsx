f7cf4d2814b7a43ce40d7ec53291a7f8cb9f675b
Pato <pwzarauz@gmail.com>
2025-09-21 17:18:05 -0300

    Updated styles to add new dropdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Button from "./components/Button"
 import styles from "./App.module.scss"
+import Dropdown from "./components/Dropdown"
 
 export const App = () => {
   return (
     <div className={styles.container}>
       <div>
         <Button text="Search" />
+        <Dropdown label="Units" />
       </div>
     </div>
   )
